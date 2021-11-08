b36949f7cd129fc110c2e1aa5fbec3ed3cd2d974
amrobin99 <amrobin@sas.upenn.edu>
2021-11-08 12:56:49 -0500

    fanpage update

---
+++ exercises/javascript/assets/main.js
@@ -0,0 +1,9 @@
+let body = document.body;
+
+function turnOnLight() {
+  body.classList.add("light");
+  button.classList.add("buttonOn");
+}
+
+let button = document.querySelector(".button");
+button.addEventListener("click", turnOnLight);
