cfc9a689664229a6617014cd01e90b32b473aa67
amrobin99 <amrobin@sas.upenn.edu>
2021-11-17 11:37:36 -0500

    update 1117

--- exercises/p5/assets/js/sketch.js
+++ exercises/p5/assets/js/sketch.js
@@ -1,31 +1,24 @@
 
-let slider;
+let slider; 
 
 function setup() {
-  //create a variable for canvas so that we can reference it for positioning later
   let canvas = createCanvas(windowWidth, windowHeight);
-  //you can specify the position of your element like this. it's best to do most positioning in CSS, but here I'm setting it to the top
   canvas.position(0, 0);
-  //you can add a CSS class to something by putting .class after it
   canvas.class("container");
-  //set the variable to the createSlider function. createSlider values are: minimum, maximum, starting value.
   slider = createSlider(10, 1000, 0);
   slider.class("slider");
 
- let txt = createDiv('');
-  txt.class("copy")
-  txt.position(50, 50);
-}
 function draw() {
   background("greenyellow");
-
   fill("white");
-  
   translate(580, 400);
   noStroke();
   for (let i = 0; i < 10; i ++) {
     ellipse(0, 60, 40, 160);
     rotate(PI/5);
+
+  if mousePressed (function fill(random))
   }
 
 }
+
