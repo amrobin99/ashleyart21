5b502d6977e8864ef00bbe30b63b6b805b14e314
amrobin99 <amrobin@sas.upenn.edu>
2021-12-06 13:11:08 -0500

    12/6 update

--- projects/data/assets/main.js
+++ projects/data/assets/main.js
@@ -1,11 +1,20 @@
 let button = document.querySelector(".button");
-let ads = ["images/usps.png", "images/family.png", "images/frito.png", "images/ulta.png"];
+let ads = ["assets/images/blue.png", "assets/images/green.png", "assets/images/red.png", "assets/images/yellow.png"];
 let body = document.querySelector("body");
 
-function newAd() {
-  let randIndex = Math.floor(Math.random() * ads.length);
-  let randAd = ads[randIndex];
-  body.style.backgroundImage = "url(" + randAd + ")";
-}
+// function newAd() {
+//   let randIndex = Math.floor(Math.random() * ads.length);
+//   let randAd = ads[randIndex];
+//   body.style.backgroundImage = "url(" + randAd + ")";
+//   console.log(randAd);
+// }
 
-button.addEventListener("click", newAd);
+
+// button.addEventListener("click", newAd);
+
+button.onclick =
+document.getElementById("button").onclick = function() {newAd()};
+
+function newAd() {
+  document.querySelector("button").innerHTML = ads 1;
+}
